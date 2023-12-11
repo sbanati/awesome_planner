f0d2f9329a7777cc49cd4d8d8daed493879d054a
smbanati-IZxCKm <smbanati@gmail.com>
2023-12-11 14:45:36 -0500

    added a function to DOM manipulate and create html elements dynamically. Need to add a function to add the colors but the frame is there

--- script.js
+++ script.js
@@ -1,7 +1,97 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function() {
+  // Scoped declarations within the document ready function
+  let today = dayjs();// Initiates dayjs library
+  let timeDisplayEl = $('#currentDay'); // links element to id
+  let timeBlocksContainer = $('.container-lg');// links element to class
+  const businessHours = ['9AM', '10AM', '11AM', '12PM','1PM','2PM','3PM','4PM','5PM'];
+  let currentHour = today.hour()
+  // Function to display the time and date
+  
+  
+  
+  
+  // function to display time and date 
+  function displayTime() {
+      //Sets rightNow to show the time using dayjs in desired format
+      let rightNow = dayjs().format('dddd, MMMM, YYYY [|] h:mm:ss a');
+      // Call the element and display the text content document
+      timeDisplayEl.text(rightNow);
+    
+    } 
+  
+  
+  
+  
+  
+  
+  function createTimeBlock () {
+    // Create the time block html elements and add class 
+    // Append the time block elements 
+    // Maybe we generate the elements using loop to create the elements on the fly
+    for (let i = 0; i < businessHours.length; i++) {
+      // Create new time block element
+      let timeBlock = $('<div>').addClass('row time-block');
 
+      // Create the hourColumn element 
+      let hourColumn = $('<div>').addClass('col-2 col-md-1 hour text-center py-3');
+      // Get text content for element
+      hourColumn.text(businessHours[i]);
+      // append the element 
+      timeBlock.append(hourColumn);
+      
+      let descriptionTextarea = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', '3');
+      timeBlock.append(descriptionTextarea);
+      
+      // create save button and icon and add class
+      let saveButton = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
+      let icon = $('<i>').addClass('fas fa-save').attr('aria-hidden', 'true');
+      // Append icon to saveButton and then saveButton to timeBlock
+      saveButton.append(icon);
+      timeBlock.append(saveButton);
+      
+      
+      timeBlocksContainer.append(timeBlock);
+      
+      
+      // Extract the hour from the businessHours array
+      let blockHour = parseInt(businessHours[i]);
+      
+      // Adjust blockHour to 24-hour format (1PM and 2PM become 13 and 14)
+      if (businessHours[i].includes('PM') && blockHour !== 12) {
+          blockHour += 12;
+      }
+      
+    }
+    
+  }      
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Calls displayTime function
+  displayTime();
+  // Updates the time interval every 1 second
+  setInterval(displayTime, 1000);
+  // Calls createTimeBlock function
+  createTimeBlock();
+
+});
 
 
 
